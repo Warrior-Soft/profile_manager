ba12db24c8377239d429f8b1b8826132443bc2ab
Brayan Guerrero <boss280498@gmail.com>
2023-01-01 12:26:46 -0400

    React router instalado

--- react-app/profile-manager/src/App.js
+++ react-app/profile-manager/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import List  from "./components/List";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <div className="App">
        <h1>Hola mundo</h1>
+       <List></List>
     </div>
   );
 }
